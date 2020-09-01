4e419dab987231200d03f8e5df0150215fd82aba
aaryan0409 <70377992+aaryan0409@users.noreply.github.com>
2020-09-01 08:09:35 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+var movingrect,fixedrect;
+
+function setup() {
+  createCanvas(800,400);
+  movingrect=createSprite(400, 200, 50, 50);
+  movingrect.shapeColor="blue";
+  fixedrect=createSprite(300,100,50,50);
+  fixedrect.shapeColor="orange";
+}
+
+function draw() {
+  background("black");
+  movingrect.x=mouseX;
+  movingrect.y=mouseY;
+  if (movingrect.x-fixedrect.x<movingrect.width/2+fixedrect.width/2&&
+    fixedrect.x-movingrect.x<movingrect.width/2+fixedrect.width/2&&
+    movingrect.y-fixedrect.y<movingrect.height/2+fixedrect.height/2&&
+    fixedrect.y-movingrect.y<movingrect.height/2+fixedrect.height/2) {
+      movingrect.shapeColor="red";
+      fixedrect.shapeColor="red";
+    }   else {
+      movingrect.shapeColor="blue";
+      fixedrect.shapeColor="orange";
+    }
+  drawSprites();
+}
